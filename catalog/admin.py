62006d3bc53aefd1e1d0269fe7870e125d6ca52f
alejandrojesuschaconvargas-cmyk <alejandrojesuschaconvargas@gmail.com>
2026-03-20 02:31:09 +0000

    models added to admin.py

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Artist, Album, Track, Playlist, PlaylistTrack
 
-# Register your models here.
+admin.site.register(Artist)
+admin.site.register(Album)
+admin.site.register(Track)
+admin.site.register(Playlist)
+admin.site.register(PlaylistTrack)
