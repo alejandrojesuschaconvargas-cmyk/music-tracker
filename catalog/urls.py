5f0e5a6e54b8328ff322a75c31037b9dff5c4429
alejandrojesuschaconvargas-cmyk <alejandrojesuschaconvargas@gmail.com>
2026-03-21 18:19:52 +0000

    catalog/urls.py API updated

--- catalog/urls.py
+++ catalog/urls.py
@@ -7,5 +7,13 @@ urlpatterns = [
     path("tracks/<int:track_id>/", views.track_detail, name="track_detail"),
     path("playlists/", views.playlist_list, name="playlist_list"),
     path("playlists/<int:playlist_id>/", views.playlist_detail, name="playlist_detail"),
+
+    #APIs
+    path("api/artists/", views.api_artist_list, name="api_artist_list"),
+    path("api/albums/", views.api_album_list, name="api_album_list"),
+    path("api/tracks/", views.api_track_list, name="api_track_list"),
+    path("api/tracks/<int:track_id>/", views.api_track_detail, name="api_track_detail"),
+    path("api/playlists/", views.api_playlist_list, name="api_playlist_list"),
+    path("api/playlists/<int:playlist_id>/", views.api_playlist_detail, name="api_playlist_detail"),
 ]
 
